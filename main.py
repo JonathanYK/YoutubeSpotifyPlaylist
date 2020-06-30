8fe8faa8c9c64279e6c108c1b7fce2a87c8e2dac
JonathanYK <yk1993@gmail.com>
2020-06-30 22:21:35 +0300

    added createPlaylist function and usrDetails.py

--- main.py
+++ main.py
@@ -1,25 +1,60 @@
+import json
+import usrDetails
+import requests
 
-class createPlaylist:
+class Playlist:
 
     def __init__(self):
-        pass
+        self.userId = usrDetails.spotifyUserId
+        self.spotifyToken = usrDetails.spotifyToken
+
 
     # Log in Youtube
     def getYouTubeClient(self):
         pass
 
+
     # Get all the relevant videos
     def getLikedVideos(self):
         pass
 
+
     # Create new playlist in Spotify
     def createPlaylist(self):
-        pass
+        requestBody = json.dumps({
+            "name": "Imported YouTube Playlist",
+            "description": "Imported automatically by Yonatan Kalma",
+            "public": True
+        })
+
+        query = "https://api.spotify.com/v1/users/{}/playlists".format(usrDetails.spotifyToken)
+        response = requests.post(
+            query,
+            data=requestBody,
+            headers={
+                "Content-Type": "application/json",
+                "Authorization": "Bearer {}".format(usrDetails.spotifyToken)
+            }
+        )
+        responseJson = response.json()
+
+        # Playlist id:
+        return responseJson["id"]
+
+
 
     # Search for video
-    def getSpotifyUri(self):
-        pass
+    def getSpotifyUri(self, songName, artist):
+
+        query = 1
+
 
     # Add the song to the playlist
     def addSongToPlaylist(self):
         pass
+
+
+
+
+a = Playlist()
+a.createPlaylist()
---
+++ usrDetails.py
@@ -0,0 +1,4 @@
+# Make sure to fill the corresponding fields:
+
+spotifyToken = "bf77321a01944ec89c39bcf0e886a9cb"
+spotifyUserId = "ce039c6896574f7f9b658098f9dddf54"

c1cccaafad07ecfe30c0abb52ffdd6e350d1f057
JonathanYK <yk1993@gmail.com>
2020-06-30 14:05:47 +0300

    Functions declared on main.py

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-#addd
+
+class createPlaylist:
+
+    def __init__(self):
+        pass
+
+    # Log in Youtube
+    def getYouTubeClient(self):
+        pass
+
+    # Get all the relevant videos
+    def getLikedVideos(self):
+        pass
+
+    # Create new playlist in Spotify
+    def createPlaylist(self):
+        pass
+
+    # Search for video
+    def getSpotifyUri(self):
+        pass
+
+    # Add the song to the playlist
+    def addSongToPlaylist(self):
+        pass
